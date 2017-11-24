c8a00e24cec53ef9ed4a2c35b8d79ea516274813
airscrat <hsq2013@163.com>
2017-11-24 15:16:53 +0800

    commit

---
+++ EAWpfSkins/Themes/Diamond/DMWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace EAWpfSkins.Themes.Diamond
+{
+    public partial class DMWindow
+    {
+        void Minimized(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(sender as FrameworkElement).WindowState = WindowState.Minimized;
+        }
+        void Normal(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(sender as FrameworkElement).WindowState = WindowState.Normal;
+        }
+        void Maximized(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(sender as FrameworkElement).WindowState = WindowState.Maximized;
+        }
+        void Close(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(sender as FrameworkElement).Close();
+        }
+    }
+}
---
+++ EAWpfSkins/Themes/Theme.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace EAWpfSkins.Themes
+{
+    public class Theme
+    {
+        public static void Switch(Visual pVisual)
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(pVisual); i++)
+            {
+                Visual childVisual = (Visual)VisualTreeHelper.GetChild(pVisual, i);
+                if (childVisual != null)
+                {
+                    Controls.Utility.Refresh(childVisual as FrameworkElement);
+                    Switch(childVisual);
+                }
+            }
+        }
+    }
+}

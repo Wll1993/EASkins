2d79c6f823f0829765f3a4d0ca9b4d30821f3c4f
airscrat <hsq2013@163.com>
2017-11-30 14:34:33 +0800

    commit

--- EASkins/Properties/AssemblyInfo.cs
+++ EASkins/Properties/AssemblyInfo.cs
@@ -32,11 +32,9 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //主题特定资源词典所处位置
-    //(当资源未在页面
-    //或应用程序资源字典中找到时使用)
+    //(当资源未在页面//或应用程序资源字典中找到时使用)
     ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
-    //(当资源未在页面
-    //、应用程序或任何主题专用资源字典中找到时使用)
+    //(当资源未在页面、应用程序或任何主题专用资源字典中找到时使用)
 )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
--- EAWpfSkins/Properties/AssemblyInfo.cs
+++ EAWpfSkins/Properties/AssemblyInfo.cs
@@ -32,11 +32,9 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //主题特定资源词典所处位置
-    //(当资源未在页面
-    //或应用程序资源字典中找到时使用)
+    //(当资源未在页面或应用程序资源字典中找到时使用)
     ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
-    //(当资源未在页面
-    //、应用程序或任何主题专用资源字典中找到时使用)
+    //(当资源未在页面、应用程序或任何主题专用资源字典中找到时使用)
 )]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID

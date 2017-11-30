03c984976ca89c4ea69e47ba9bc92333d882c21e
airscrat <hsq2013@163.com>
2017-11-30 14:35:44 +0800

    commit

--- EASkins/Properties/AssemblyInfo.cs
+++ EASkins/Properties/AssemblyInfo.cs
@@ -20,23 +20,6 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//若要开始生成可本地化的应用程序，请
-//<PropertyGroup> 中的 .csproj 文件中
-//例如，如果您在源文件中使用的是美国英语，
-//使用的是美国英语，请将 <UICulture> 设置为 en-US。  然后取消
-//对以下 NeutralResourceLanguage 特性的注释。  更新
-//以下行中的“en-US”以匹配项目文件中的 UICulture 设置。
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //主题特定资源词典所处位置
-    //(当资源未在页面//或应用程序资源字典中找到时使用)
-    ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
-    //(当资源未在页面、应用程序或任何主题专用资源字典中找到时使用)
-)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f9adee9d-e37b-460d-828d-ef1775207e80")]
 

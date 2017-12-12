15a6d5bd33167a85b4d0a42a5867950d863c8819
airscrat <hsq2013@163.com>
2017-12-12 18:16:45 +0800

    commit

---
+++ EAStyles/Controls/MiStyle/MiButton.cs
@@ -0,0 +1,23 @@
+﻿using EAStyles.Utilitys.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EAStyles.Controls.MiStyle
+{
+    public class MiButton : Button
+    {
+        public MiButton()
+        {
+            Controls.ControlUtility.Refresh(this);
+        }
+
+        static MiButton()
+        {
+            ElementBase.DefaultStyle<MiButton>(DefaultStyleKeyProperty);
+        }
+    }
+}

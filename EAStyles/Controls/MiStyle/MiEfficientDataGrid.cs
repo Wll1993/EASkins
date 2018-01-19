1d430b4b4969d03f3bbb0df1049e8677ad15a881
airscrat <hsq2013@163.com>
2018-01-19 13:34:38 +0800

    commit

---
+++ EAStyles/Controls/MiStyle/MiEfficientDataGrid.cs
@@ -0,0 +1,21 @@
+﻿using EAStyles.Utilitys;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EAStyles.Controls.MiStyle
+{
+    public class MiEfficientDataGrid:DataGrid
+    {
+        public MiEfficientDataGrid()
+        {            
+            ResourceDictionary styleRes = new ResourceDictionary();
+            styleRes.Source = new Uri("/EAStyles;component/Themes/MiStyle/MiEfficientDataGrid.xaml",
+                    UriKind.RelativeOrAbsolute);
+            System.Windows.Style dataGridStyle = styleRes["miEffiDataGrid"] as Style;
+            this.SetValue(MiEfficientDataGrid.StyleProperty, dataGridStyle);
+
+            ControlUtility.Refresh(this);
+        }
+    }
+}
